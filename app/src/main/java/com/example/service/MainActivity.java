3d5585c0ebeda29f8874e8023e513a33a7690519
Iron <perts.pasha.pro@gmail.com>
2020-05-09 11:17:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/service/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.service;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editTextInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextInput = findViewById(R.id.edit_text_input);
+    }
+
+    public void startService(View v) {
+        String input = editTextInput.getText().toString();
+
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        serviceIntent.putExtra("inputExtra", input);
+
+        ContextCompat.startForegroundService(this, serviceIntent);
+    }
+
+    public void stopService(View v) {
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        stopService(serviceIntent);
+    }
+}
